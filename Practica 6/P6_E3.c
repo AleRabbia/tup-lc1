f5c7b2a7013eba988896548ad79f6a5c438d825b
Alexis Rabbia <alexisrabbia@gmail.com>
2023-06-22 07:23:59 -0300

    Ejercicio 3 cadenas

---
+++ Practica 6/P6_E3.c
@@ -0,0 +1,21 @@
+/*3. Crear una función que devuelva la primera letra de una cadena de texto.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char Primera_letra(char *cadena)
+{
+    return *cadena;
+}
+
+void main()
+{
+    char cadena[] = " ";
+    printf("Ingrese una cadena:\n");
+    scanf("%s", &cadena);
+    char letra = Primera_letra(cadena);
+    printf("La primera letra de la cadena es: %c\n", letra);
+    system("pause");
+}
